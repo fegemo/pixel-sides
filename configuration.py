1daa9a9875685b582420ef56812631287038e013
Flávio Coutinho <fegemo@gmail.com>
2022-07-29 15:29:20 -0300

    Adds a large miscellaneous dataset

--- configuration.py
+++ configuration.py
@@ -6,12 +6,13 @@ SEED = 47
 DATA_FOLDERS = [
     os.sep.join(["datasets", folder])
                 for folder
-                in ["tiny-hero", "rpg-maker-2000", "rpg-maker-xp", "rpg-maker-vxace"]
+                in ["tiny-hero", "rpg-maker-2000", "rpg-maker-xp", "rpg-maker-vxace", "miscellaneous"]
 ]
 
 DIRECTION_FOLDERS = ["0-back", "1-left", "2-front", "3-right"]
-DATASET_MASK = [0, 0, 1, 0]
-DATASET_SIZES = [912, 216, 294, 408]
+DATASET_MASK = [0, 0, 1, 0, 0]
+# DATASET_MASK = [1, 1, 1, 1, 0]
+DATASET_SIZES = [912, 216, 294, 408, 12372]
 DATASET_SIZES = [n*m for n, m in zip(DATASET_SIZES, DATASET_MASK)]
 
 DATASET_SIZE = sum(DATASET_SIZES)
@@ -19,10 +20,11 @@ TRAIN_PERCENTAGE = 0.85
 TRAIN_SIZES = [ceil(n * TRAIN_PERCENTAGE) for n in DATASET_SIZES]
 TRAIN_SIZE = sum(TRAIN_SIZES)
 TEST_SIZES = [DATASET_SIZES[i] - TRAIN_SIZES[i] for i, n in enumerate(DATASET_SIZES)]
+# TEST_SIZES = [0, 0, 44, 0, 0]
 TEST_SIZE = sum(TEST_SIZES)
 
 BUFFER_SIZE = DATASET_SIZE
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 
 IMG_SIZE = 64
 INPUT_CHANNELS = 4
